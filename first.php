0afc77e038755c9c7e9f7373a2ed464c41a0f367
Aslan Shaikh <aslanshaikhgec@gmail.com>
2021-12-20 10:21:07 +0530

    changes made in home page ..
    images added in cards .. of equal length

--- first.php
+++ first.php
@@ -11,6 +11,11 @@
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    <style>
+        h3 {
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
@@ -147,33 +152,29 @@
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="images/venue.jpg" class="d-block w-100" alt="...">
+                <img src="images/carou1.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h2>Welcome to A.company</h2>
-                    <p>Technology, news, blogs</p>
-                    <button type="button" class="btn btn-primary">Technology</button>
-                    <button type="button" class="btn btn-secondary">Web devp</button>
-                    <button type="button" class="btn btn-dark">Quick shorts</button>
+
+                    <button type="button" class="btn btn-primary">Know More</button>
+                    <button type="button" class="btn btn-dark">Quick Look</button>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="images/creta.jpg" class="d-block w-100" alt="...">
+                <img src="images/carou2.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h2>Welcome to A.company</h2>
-                    <p>Technology, news, blogs</p>
-                    <button type="button" class="btn btn-primary">Technology</button>
-                    <button type="button" class="btn btn-secondary">Web devp</button>
-                    <button type="button" class="btn btn-dark">Quick shorts</button>
+                    <p></p>
+                    <button type="button" class="btn btn-primary">Know More</button>
+                    <button type="button" class="btn btn-dark">Quick Look</button>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="https://source.unsplash.com/random/1000x300/?coding" class="d-block w-100" alt="...">
+                <img src="https://source.unsplash.com/random/800x300/?cars" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h2>Welcome to A.company</h2>
-                    <p>Technology, news, blogs</p>
-                    <button type="button" class="btn btn-primary">Technology</button>
-                    <button type="button" class="btn btn-secondary">Web devp</button>
-                    <button type="button" class="btn btn-dark">Quick shorts</button>
+                    <button type="button" class="btn btn-primary">Know More</button>
+                    <button type="button" class="btn btn-dark">Quick Look</button>
                 </div>
             </div>
         </div>
@@ -188,88 +189,119 @@
             <span class="visually-hidden">Next</span>
         </button>
     </div>
+    <hr>
+    <br>
+    <br>
+    <h3>Models</h3>
 
 
-    <div class="containercard">
+    <div class="containercard my-10">
 
         <div class="card-group">
             <div class="card">
-                <img class="card-img-top" src="images/elantra.jpg" alt="Card image cap">
+                <img class="card-img-top" src="images/santro.png" alt="Card image cap">
                 <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <a href="">know more </a>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-    </div>
-    <div class="card">
-        <img class="card-img-top" src="images/gi10.jpg" alt="Card image cap">
-          <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-              <a href="">know more </a>
+                    <h5 class="card-title">Hyundai Santro</h5>
+                    <p class="card-text">Hyundai Santro is a 5 seater Hatchback available in a price range of ₹ 4.77 -
+                        6.45 Lakh. It is available in 9 variants, 1 engine option and 2 transmission options : Manual
+                        and AMT. The Santro is available in 5 colours. The mileage of Santro ranges from 20 km/kg to 30
+                        km/kg.
+                        Engine: 1086 cc
+                        Price: ₹ 4.77 Lakh onwards
+                        Fuel Type: Petrol & CNG
+                        Seating Capacity: 5 Seater</p>
+                    <!-- <a href="">know more </a> -->
+                    <a class="btn btn-outline-primary" href="first.php" role="button">Book Test-Drive</a>
+                    <a class="btn btn-outline-primary" href="about.php" role="button">Buy Now</a>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="images/creta.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <a href="">know more </a>
-            </div>
-          <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-    </div>
+            <div class="card">
+                <img class="card-img-top" src="images/gi10nios.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Grand i10 nios</h5>
+                    <p class="card-text">Hyundai Grand i10 Nios Era mileage : It returns a certified mileage of 20.7
+                        kmpl. ... Hyundai Grand i10 Nios Era Engine and Transmission: It is powered by a 1197 cc engine
+                        which is available with a Manual transmission. The 1197 cc engine puts out 81.86bhp@6000rpm of
+                        power and 113.75Nm@4000rpm of torque.
+                        Mileage (upto): 20.7 kmpl
+                        Transmission: Manual
+                        Engine (upto): 1197 cc
+                        Seats: 5
+                    </p>
 
-    <div class="card-group">
-        <div class="card">
-            <img class="card-img-top" src="images/tuscon.jpg" alt="Card image cap">
-            <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="">know more </a>
-    </div>
-    <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-</div>
-<div class="card">
-    <img class="card-img-top" src="images/venue.jpg" alt="Card image cap">
-      <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <a href="">know more </a>
+                    <a class="btn btn-outline-primary" href="first.php" role="button">Book Test-Drive</a>
+                    <a class="btn btn-outline-primary" href="about.php" role="button">Buy Now</a>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="images/creta2.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Creta</h5>
+                    <p class="card-text">The Hyundai Creta has 1 Diesel Engine and 2 Petrol Engine on offer. The Diesel engine is 1493 cc while the Petrol engine is 1497 cc and 1353 cc . It is available with Manual & Automatic transmission.Depending upon the variant and fuel type the Creta has a mileage of 16.8 to 21.4 kmpl . The Creta is a 5 seater 4 cylinder car and has length of 4300mm, width of 1790mm and a wheelbase of 2610mm.</p>
+                        <a class="btn btn-outline-primary" href="first.php" role="button">Book Test-Drive</a>
+                        <a class="btn btn-outline-primary" href="about.php" role="button">Buy Now</a>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
         </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+
+        <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="images/elantra2.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Elantra</h5>
+
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                        <a class="btn btn-outline-primary" href="first.php" role="button">Book Test-Drive</a>
+                        <a class="btn btn-outline-primary" href="about.php" role="button">Buy Now</a>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="images/i20.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">I20</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.
+                    </p>
+                    <a class="btn btn-outline-primary" href="first.php" role="button">Book Test-Drive</a>
+                    <a class="btn btn-outline-primary" href="about.php" role="button">Buy Now</a>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="images/tucson2.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Tucson</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to show that equal height
+                        action.</p>
+                        <a class="btn btn-outline-primary" href="first.php" role="button">Book Test-Drive</a>
+                        <a class="btn btn-outline-primary" href="about.php" role="button">Buy Now</a>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
         </div>
+
     </div>
-    <div class="card">
-        <img class="card-img-top" src="images/verna.jpg" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <a href="">know more </a>
-        </div>
-      <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-    </div>
-</div>
-    
-</div>
     <!-- footer  -->
     <!-- Footer -->
     <footer class="page-footer font-small blue">
-        
+
         <!-- Copyright -->
         <div class="footer-copyright text-center py-3">© 2022 Copyright:
             <a href="index.html"> ACompany.com</a>
