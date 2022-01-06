a5ab74309d292af8b41be75840c4bab6702993ff
MaStErMiNd6001 <you@example.com>
2022-01-06 10:26:38 +0530

    add image to car details

--- adminlogin/adminmain.php
+++ adminlogin/adminmain.php
@@ -44,18 +44,10 @@ if(!isset($_SESSION['user'])) {
           <th>features</th>
           <th>images</th>
           <th>DELETE</th>
-          <th>UPDATE</th>
 
         </tr>
 
         <?php
-
-          // $con = mysqli_connect('localhost','root');
-          // // check connection
-          //   if(!$con){
-          //     echo 'Connection error: '. mysqli_connect_error();
-          //  }
-          //   mysqli_select_db($con, 'car');
           include'conn.php';
             $q = "select * from cardet";
 
@@ -80,36 +72,32 @@ if(!isset($_SESSION['user'])) {
             <?php echo $res['features'];  ?>
           </td>
           <td>
-          <?php echo $res['images']; ?>
+          <img  src="../<?php echo ($res['images']); ?>" alt="Card image cap">
           </td>
           <!-- <td> -->
-          <!-- <?php /*
+            <!-- <?php /*
           <img  src="<?php echo htmlspecialchars($res['images']); ?>" alt="Card image cap"> -->
           */?>
           </td> -->
           <td>  <a class="btn btn-danger"href="delete.php?carid=<?php echo $res['carid']; ?>">delete</a>
-            </td>
-
-          <td> <button class="btn btn-primary"> <a href="update.php?id=<?php echo $res['carid']; ?>"
-                class="text-white">update</a>
-            </button></td>
-
-        </tr>
-        <?php
+        </td>
+        
+              
+            </tr>
+            <?php
                  }
-                ?>
+                 ?>
       </table>
     </div>
   </div>
   <a href="insert.php" class=" btn btn-danger">insert new car </a>
   
   <a href="..\loginsign\logout.php" class=" btn btn-danger">logout </a>
-  <?php $res = mysqli_fetch_array($query) ?>
-  <img  src="<?php echo htmlspecialchars($res[101]['images']); ?>" alt="Card image cap">
+  
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
     integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
